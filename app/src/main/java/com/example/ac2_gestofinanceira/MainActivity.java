3f42e1a9e6a89ca128c1ac607e7d5f21ee54c511
Leonardo Lima <104847717+sirbickle@users.noreply.github.com>
2024-05-09 21:04:51 -0300

    teste

--- app/src/main/java/com/example/ac2_gestofinanceira/MainActivity.java
+++ app/src/main/java/com/example/ac2_gestofinanceira/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.ac2_gestofinanceira;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,26 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-// LEONARDO DANIEL LIMA DE VERAS, RA- 212143
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        TextView txt = findViewById(R.id.txtText1);
-        txt.setText("teste");
+        
 
-        Button btn = findViewById(R.id.btn1);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Botão Clicado", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        }
 
     }
+}
